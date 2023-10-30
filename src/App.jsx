d917203d70a8b5a1bec169b3f607a1ff06355565
Henry Agu <aguhenrychuks@gmail.com>
2023-10-30 22:11:33 +0100

    feat:hero section of homepage  for desktop updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,23 @@
 // stylesheet
 import "./App.scss";
 
-// react-helmet
-import { Helmet } from "react-helmet";
+
 
 // react routers
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // components
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <main>
-      {/* React Helmet */}
-      <Helmet>
-        {/* Basic Meta Tags */}
-        <title>Welcome To WorkMan Website | WorkMan</title>
-        <meta
-          name="description"
-          content="WorkMan is my final year project at the University of Port Harcourt. It is a website that connects users to artisans residing at Choba area."
-        />
-        <meta
-          name="keywords"
-          content="artisan, craftsmen, handmade, custom-made, traditional crafts, unique art, bespoke, handcrafted goods, artisanal products, local artisans, craftsmanship, handmade jewelry, handmade furniture, artisan marketplace, artisan community, creative artisans, artisanal services"
-        />
-
-        <meta name="author" content="Agu Henry" />
-
-        {/* Open Graph Meta Tags (for social media sharing) */}
-        <meta property="og:title" content="Welcome To WorkMan | WorkMan" />
-        <meta
-          property="og:description"
-          content="WorkMan is my final year project at the University of Port Harcourt. It is a website that connects users to artisans residing at Choba area."
-        />
-        <meta property="og:image" content="./assets/OG-image.svg" />
-        <meta property="og:url" content="https://workman-six.vercel.app/" />
-
-        {/* Twitter Meta Tags (for Twitter Cards) */}
-        <meta name="twitter:card" content="./assets/OG-image.svg" />
-        <meta name="twitter:title" content="Welcome To WorkMan | WorkMan" />
-        <meta
-          name="twitter:description"
-          content="WorkMan is my final year project at the University of Port Harcourt. It is a website that connects users to artisans residing at Choba area."
-        />
-        <meta name="twitter:image" content="./assets/OG-image.svg" />
-      </Helmet>
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
       </BrowserRouter>
     </main>
   );
---
+++ src/pages/Home/components/Hero/Hero.jsx
@@ -0,0 +1,50 @@
+// stylesheet
+import "./style/Hero.scss";
+
+// react router dom
+import { NavLink } from "react-router-dom";
+
+// images
+import HeroImage from "./images/hero.png";
+import GroupImage from "./images/group.png";
+import searchIcon from "./images/search.svg";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="left-hero">
+        <div className="hero-header">
+          <h1>
+            Find the <span>Best Artisans</span>
+          </h1>
+          <h1>in Choba</h1>
+        </div>
+        <div className="hero-body">
+          <p>
+            Thousands of professional artisans in the fashion designer, sales,
+            engineering and technology sectors are waiting to help offer top
+            notch services for your business
+          </p>
+        </div>
+        <div className="hero-trusted">
+          <img src={GroupImage} alt="Group Image" />
+          <p>Trusted by over 2,000 Buinesses</p>
+        </div>
+        <div className="hero-search">
+          <div className="search-box">
+            <img src={searchIcon} alt="icon" />
+            <input type="text" placeholder="What position are you looking for ?"/>
+          </div>
+          <NavLink to="">
+            <button>Find Artisan</button>
+          </NavLink>
+        </div>
+      </div>
+      <div className="right-hero">
+        <img src={HeroImage} alt="Hero image" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
