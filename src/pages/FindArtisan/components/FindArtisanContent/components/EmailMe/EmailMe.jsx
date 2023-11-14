1161ca212789db59b68658c03fd2f1374c7b6c94
Henry Agu <aguhenrychuks@gmail.com>
2023-11-14 21:49:22 +0100

    feat: email me component updated

--- src/pages/FindArtisan/components/FindArtisanContent/components/EmailMe/EmailMe.jsx
+++ src/pages/FindArtisan/components/FindArtisanContent/components/EmailMe/EmailMe.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+// stylesheet
+import "./style/EmailMe.scss";
+
+// images
+import EmailIcon from "./images/Email.png";
 
 const EmailMe = () => {
   return (
-    <div>EmailMe</div>
-  )
-}
+    <div className="email-me">
+      <div className="email-me-header">
+        <img src={EmailIcon} alt="Email" />
+        <h4>Email me for jobs</h4>
+      </div>
+      <p>
+        Ut esse eiusmod aute. Sit enim labore dolore. Aute ea fugiat commodo ea
+        foes.
+      </p>
+      <div className="submit-email">
+        <input type="email" placeholder="name@mail.com" />
+        <button>Subcribe</button>
+      </div>
+    </div>
+  );
+};
 
-export default EmailMe
+export default EmailMe;
