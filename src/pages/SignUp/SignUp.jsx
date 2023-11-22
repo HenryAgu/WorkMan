083199525ce4488d5169cb6cf7fe2ea9e8d756dd
Henry Agu <aguhenrychuks@gmail.com>
2023-11-22 21:34:38 +0100

    feat: sign up page upated

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,15 +1,55 @@
 // stylesheet
 import "./style/SignUp.scss";
 
+// react-helmet
+import { Helmet } from "react-helmet";
+
 // components
 import LoginHeader from "../Login/components/LoginHeader/LoginHeader";
+import SignUpForm from "./component/SignUpForm/SignUpForm";
 
 const SignUp = () => {
   return (
-    <div className="sign-up">
-      <LoginHeader/>
-    </div>
-  )
-}
+    <>
+      {/* React Helmet */}
+      <Helmet>
+        {/* Basic Meta Tags */}
+        <title>Sign Up As Customer | WorkMan</title>
+        <meta
+          name="description"
+          content="WorkMan is my final year project at the University of Port Harcourt. It is a website that connects users to artisans residing at Choba area."
+        />
+        <meta
+          name="keywords"
+          content="artisan, craftsmen, handmade, custom-made, traditional crafts, unique art, bespoke, handcrafted goods, artisanal products, local artisans, craftsmanship, handmade jewelry, handmade furniture, artisan marketplace, artisan community, creative artisans, artisanal services"
+        />
 
-export default SignUp
+        <meta name="author" content="Agu Henry" />
+
+        {/* Open Graph Meta Tags (for social media sharing) */}
+        <meta property="og:title" content="Sign Up As Customer | WorkMan" />
+        <meta
+          property="og:description"
+          content="WorkMan is my final year project at the University of Port Harcourt. It is a website that connects users to artisans residing at Choba area."
+        />
+        <meta property="og:image" content="./assets/OG-image.svg" />
+        <meta property="og:url" content="https://workman-six.vercel.app/" />
+
+        {/* Twitter Meta Tags (for Twitter Cards) */}
+        <meta name="twitter:card" content="./assets/OG-image.svg" />
+        <meta name="twitter:title" content="Sign Up As Customer | WorkMan" />
+        <meta
+          name="twitter:description"
+          content="WorkMan is my final year project at the University of Port Harcourt. It is a website that connects users to artisans residing at Choba area."
+        />
+        <meta name="twitter:image" content="./assets/OG-image.svg" />
+      </Helmet>
+      <div className="sign-up">
+        <LoginHeader />
+        <SignUpForm />
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
---
+++ src/pages/SignUp/component/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,53 @@
+// stylesheet
+import "./style/SignUpForm.scss";
+
+// images
+import SignUpImage from "./images/LoginImage.svg";
+import GoogleIcon from "./images/Google.svg";
+import FacebookIcon from "./images/Facebook.svg";
+import EmailIcon from "./images/Email.svg";
+
+// Navlinks
+import { NavLink } from "react-router-dom";
+
+const SignUpForm = () => {
+  return (
+    <div className="sign-up-form">
+      <form action="">
+        <div className="sign-up-logo">
+          <img src={SignUpImage} alt="Image" />
+        </div>
+        <div className="sign-up-header">
+          <h1>Sign up</h1>
+        </div>
+        <div className="sign-up-options">
+          <button>
+            <img src={GoogleIcon} alt="icon" />
+            <p>Continue with Google</p>
+          </button>
+          <button>
+            <img src={FacebookIcon} alt="icon" />
+            <p>Continue with Facebook</p>
+          </button>
+          <NavLink>
+            <button>
+              <img src={EmailIcon} alt="icon" />
+              <p>Sign up with email</p>
+            </button>
+          </NavLink>
+        </div>
+        <div className="policy">
+          <p>
+            By signing up, you agree to the <span>Terms of Service</span> and
+            acknowledge you’ve read our <span>Privacy Policy</span>.
+          </p>
+        </div>
+        <div className="sign-in">
+          <p>Already have an account? <NavLink to="/login"><span>Log in</span></NavLink> </p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
