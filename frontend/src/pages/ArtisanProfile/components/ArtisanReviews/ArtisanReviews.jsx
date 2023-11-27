91b360bcfb4f197f2112ddff986d37878d97e583
Henry Agu <aguhenrychuks@gmail.com>
2023-11-27 22:08:06 +0100

    feat: update for frontend

--- frontend/src/pages/ArtisanProfile/components/ArtisanReviews/ArtisanReviews.jsx
+++ frontend/src/pages/ArtisanProfile/components/ArtisanReviews/ArtisanReviews.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 // stylesheet
 import "./style/ArtisanReviews.scss";
 
@@ -7,7 +8,8 @@ import reviewer from "./images/reviewer.png";
 import rateIcon from "./images/rate.svg";
 
 const ArtisanReviews = () => {
-  const reviews = [
+  const [review, setReview] = useState("");
+  const [reviewList, setReviewList] = useState([
     {
       reviewerImage: reviewer,
       rate: rateIcon,
@@ -22,14 +24,44 @@ const ArtisanReviews = () => {
       reviewMessage:
         "I couldn't be happier with the beautifully crafted kitchen cabinets. The artisan's attention to detail and commitment to quality are truly commendable. Highly recommended!",
     },
-  ];
+  ])
+  // const reviews = [
+  //   {
+  //     reviewerImage: reviewer,
+  //     rate: rateIcon,
+  //     reviewerName: "Henry Agu",
+  //     reviewMessage:
+  //       "I was impressed by the craftsmanship and attention to detail. The custom-made dining table exceeded my expectations",
+  //   },
+  //   {
+  //     reviewerImage: reviewer,
+  //     rate: rateIcon,
+  //     reviewerName: "Patrick Duru",
+  //     reviewMessage:
+  //       "I couldn't be happier with the beautifully crafted kitchen cabinets. The artisan's attention to detail and commitment to quality are truly commendable. Highly recommended!",
+  //   },
+  // ];
+
+  const handleAddReview = (event) => {
+    const newItem = {
+      id: reviewList.length + 1,
+      reviewerImage: reviewer,
+      rate: rateIcon,
+      reviewMessage: review,
+    };
+  
+    // Update the state with the new item added to the array
+    setReviewList([...reviewList, newItem]);
+    setReview("");
+  };
+  
   return (
     <div className="artisan-reviews">
       <div className="artisan-reviews-header">
         <h3>Reviews</h3>
         <img src={reviewersImages} alt="images" />
       </div>
-      {reviews.map((review,index) => (
+      {reviewList.map((review,index) => (
         <div className="review-card" key={index}>
           <div className="review-card-header">
             <div className="reviewer-image">
@@ -45,6 +77,14 @@ const ArtisanReviews = () => {
           </p>
         </div>
       ))}
+      <div className="add-review">
+        <h2>Leave A review</h2>
+        <textarea placeholder="Describe your experience" value={review} onChange={(e)=> setReview(e.target.value)}/>
+        <div className="review-info">
+          <p>Your review will be posted publicly on the web, under this artisan</p>
+        </div>
+        <button onClick={handleAddReview}>Post</button>
+      </div>
     </div>
   );
 };
