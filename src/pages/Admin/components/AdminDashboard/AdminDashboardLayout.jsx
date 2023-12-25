6d94c210ca510e4c69a3aa7ebf771edd4a2c01ec
Henry Agu <aguhenrychuks@gmail.com>
2023-12-25 13:18:37 +0100

    feat: sidebar for mobile and desktop updated

--- src/pages/Admin/components/AdminDashboard/AdminDashboardLayout.jsx
+++ src/pages/Admin/components/AdminDashboard/AdminDashboardLayout.jsx
@@ -1,16 +1,115 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+// usestate
+import { useState } from "react";
+// outlet
+import { NavLink, Outlet } from "react-router-dom";
+
+// stylesheet
+import "./style/AdminDashboardLayout.scss";
+
+// images
+import Logo from "./images/Logo.svg";
+import MenuIcon from "./images/menu.svg";
+import CloseIcon from "./images/Close.svg";
+import searchIcon from "./images/search.svg";
+import avatar from "./images/avatar.svg";
+import dashboardImage from "./images/Dashboard.svg";
+import usersImage from "./images/Users.svg";
+import artisansImage from "./images/Artisans.svg";
+import settingsImage from "./images/Settings.svg";
 
 const AdminDashboardLayout = () => {
-  return (
-    <div className='admin-dashboard-layout'>
-      <div className="side-bar">
-        
-      </div>
-      <h1>Aya</h1>
-      <Outlet/>
-    </div>
-  )
-}
+  const [showSidebar, setShowSidebar] = useState(false);
+  const sideMenuItems = [
+    {
+      id: 1,
+      menuImage: dashboardImage,
+      menuName: "Dashboard",
+      menuPath: "/admin/dashboard-overview",
+    },
+    {
+      id: 2,
+      menuImage: usersImage,
+      menuName: "Users",
+      menuPath: "/admin/users",
+    },
+    {
+      id: 3,
+      menuImage: artisansImage,
+      menuName: "Artisans",
+      menuPath: "/admin/artisans",
+    },
+    {
+      id: 4,
+      menuImage: settingsImage,
+      menuName: "Settings",
+      menuPath: "/admin/dashboard-overview",
+    },
+  ];
 
-export default AdminDashboardLayout
+  // show sidebar
+  const handleShowSidebar = () => {
+    setShowSidebar(true);
+  }
+
+  // hide sidebar
+  const handleCloseSidebar = () => {
+    setShowSidebar(false);
+  }
+  
+  return (
+    <div className="admin-dashboard-layout">
+      <div className="mobile-side-bar">
+        <div className="left-mobile-sidebar">
+          <img src={MenuIcon} alt="menu" onClick={handleShowSidebar}/>
+          <img src={Logo} alt="Logo" className="mobile-logo" />
+        </div>
+        {showSidebar ? (
+          <div className="mobile-menu">
+            <div className="mobile-menu-header">
+              <h3>Menu</h3>
+              <button onClick={handleCloseSidebar}>
+                <img src={CloseIcon} alt="icon" />
+              </button>
+            </div>
+            <ul>
+              {sideMenuItems.map((mobileMenuItem) => (
+                <NavLink to={mobileMenuItem.menuPath}>
+                  <li key={mobileMenuItem.id}>
+                    <img src={mobileMenuItem.menuImage} alt="dashboard" />
+                    <h3>{mobileMenuItem.menuName}</h3>
+                  </li>
+                </NavLink>
+              ))}
+            </ul>
+          </div>
+        ) : null}
+        <div className="right-mobile-sidebar">
+          <img src={searchIcon} alt="search" />
+          <img src={avatar} alt="avatar" />
+        </div>
+      </div>
+      <div className="side-bar">
+        <NavLink to="/admin-login">
+          <div className="logo">
+            <img src={Logo} alt="Logo" />
+          </div>
+        </NavLink>
+        <div className="side-menu">
+          {sideMenuItems.map((menu) => (
+            <NavLink to={menu.menuPath}>
+              <div className="menu-item" key={menu.id}>
+                <img src={menu.menuImage} alt="side-image" />
+                <h3 activeClassName="active">{menu.menuName}</h3>
+              </div>
+            </NavLink>
+          ))}
+        </div>
+      </div>
+      <div className="main-bar">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboardLayout;
--- src/pages/Admin/components/AdminDashboard/components/DashboardOverview/DashboardOverview.jsx
+++ src/pages/Admin/components/AdminDashboard/components/DashboardOverview/DashboardOverview.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+// react-helmet
+import { Helmet } from "react-helmet";
 
 const DashboardOverview = () => {
-  return (
-    <div>DashboardOverview</div>
-  )
-}
+  /* React Helmet */
+  <Helmet>
+    {/* Basic Meta Tags */}
+    <title>Admin Dashboard | WorkMan</title>
+    <meta
+      name="description"
+      content="WorkMan is my final year project at the University of Port Harcourt. It is a website that connects users to artisans residing at Choba area."
+    />
+    <meta
+      name="keywords"
+      content="artisan, craftsmen, handmade, custom-made, traditional crafts, unique art, bespoke, handcrafted goods, artisanal products, local artisans, craftsmanship, handmade jewelry, handmade furniture, artisan marketplace, artisan community, creative artisans, artisanal services"
+    />
 
-export default DashboardOverview
+    <meta name="author" content="Agu Henry" />
+
+    {/* Open Graph Meta Tags (for social media sharing) */}
+    <meta property="og:title" content="Admin Dashboard | WorkMan" />
+    <meta
+      property="og:description"
+      content="WorkMan is my final year project at the University of Port Harcourt. It is a website that connects users to artisans residing at Choba area."
+    />
+    <meta property="og:image" content="./assets/OG-image.svg" />
+    <meta property="og:url" content="https://workman-six.vercel.app/" />
+
+    {/* Twitter Meta Tags (for Twitter Cards) */}
+    <meta name="twitter:card" content="./assets/OG-image.svg" />
+    <meta name="twitter:title" content="Admin Dashboard | WorkMan" />
+    <meta
+      name="twitter:description"
+      content="WorkMan is my final year project at the University of Port Harcourt. It is a website that connects users to artisans residing at Choba area."
+    />
+    <meta name="twitter:image" content="./assets/OG-image.svg" />
+  </Helmet>
+  return <div>DashboardOverview</div>;
+};
+
+export default DashboardOverview;
